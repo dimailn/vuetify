c3a5676281dccd0a9eb15e06a6accbb2480c5538
Vladimir Ivanin <ivaninvv@mail.ru>
2025-08-23 15:35:53 +0300

    обновляем тесты для компонента VMain, улучшая структуру монтирования с использованием глобальных моков, добавляя новые тесты для проверки рендеринга с кастомными тегами, стилями и содержимым слотов, а также улучшая типизацию для повышения стабильности и совместимости с Vue 3

--- packages/vuetify/src/components/VMain/__tests__/VMain.spec.ts
+++ packages/vuetify/src/components/VMain/__tests__/VMain.spec.ts
@@ -2,30 +2,27 @@
 import VMain from '../VMain'
 
 // Utilities
-import {
-  mount,
-  MountOptions,
-  Wrapper,
-} from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 
 describe('VMain.ts', () => {
-  type Instance = InstanceType<typeof VMain>
-  let mountFunction: (options?: MountOptions<Instance>) => Wrapper<Instance>
+  let mountFunction: (options?: any) => any
 
   beforeEach(() => {
     mountFunction = (options = {}) => {
       return mount(VMain, {
         ...options,
-        mocks: {
-          $vuetify: {
-            application: {
-              bar: 24,
-              top: 64,
-              left: 256,
-              right: 256,
-              footer: 48,
-              insetFooter: 32,
-              bottom: 56,
+        global: {
+          mocks: {
+            $vuetify: {
+              application: {
+                bar: 24,
+                top: 64,
+                left: 256,
+                right: 256,
+                footer: 48,
+                insetFooter: 32,
+                bottom: 56,
+              },
             },
           },
         },
@@ -37,5 +34,62 @@ describe('VMain.ts', () => {
     const wrapper = mountFunction()
 
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should render custom tag', () => {
+    const wrapper = mountFunction({
+      props: {
+        tag: 'div'
+      }
+    })
+
+    expect(wrapper.element.tagName).toBe('DIV')
+  })
+
+  it('should apply correct styles based on vuetify application', () => {
+    const wrapper = mountFunction()
+    const mainElement = wrapper.element
+
+    expect(mainElement.style.paddingTop).toBe('88px') // 64 + 24
+    expect(mainElement.style.paddingRight).toBe('256px')
+    expect(mainElement.style.paddingBottom).toBe('136px') // 48 + 32 + 56
+    expect(mainElement.style.paddingLeft).toBe('256px')
+  })
+
+  it('should render with slot content', () => {
+    const wrapper = mountFunction({
+      slots: {
+        default: '<div>Test content</div>'
+      }
+    })
+
+    expect(wrapper.html()).toContain('Test content')
+  })
+
+  it('should render with complex slot content', () => {
+    const wrapper = mountFunction({
+      slots: {
+        default: [
+          '<div>First item</div>',
+          '<div>Second item</div>'
+        ]
+      }
+    })
+
+    expect(wrapper.html()).toContain('First item')
+    expect(wrapper.html()).toContain('Second item')
+  })
+
+  it('should have correct CSS classes', () => {
+    const wrapper = mountFunction()
+
+    expect(wrapper.classes()).toContain('v-main')
+    expect(wrapper.find('.v-main__wrap').exists()).toBe(true)
+  })
+
+  it('should render with default tag', () => {
+    const wrapper = mountFunction()
+
+    expect(wrapper.element.tagName).toBe('MAIN')
   })
 })
