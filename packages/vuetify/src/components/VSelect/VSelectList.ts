62ebd53128a9822776c63415f6ef3e86cca9df74
vladimir.ivanin <vladimir.ivanin@insales.ru>
2025-09-09 17:15:57 +0300

    фикс событий

--- packages/vuetify/src/components/VSelect/VSelectList.ts
+++ packages/vuetify/src/components/VSelect/VSelectList.ts
@@ -32,7 +32,6 @@ type ListTile = { item: any, disabled?: null | boolean, value?: boolean, index: 
 export default mixins(Colorable, Themeable).extend({
   name: 'v-select-list',
 
-
   props: {
     action: Boolean,
     dense: Boolean,
@@ -160,7 +159,7 @@ export default mixins(Colorable, Themeable).extend({
             acc[key] = this.$attrs[key]
           }
           return acc
-        }, {} as Record<string, any>)
+        }, {} as Record<string, any>),
       }
 
       if (!this.$slots.item) {
@@ -178,12 +177,12 @@ export default mixins(Colorable, Themeable).extend({
         parent,
         item,
         active: value,
-        attrs: attrsWithoutEvents,
+        attrs: tile,
         on: {
-          mousedown: onMousedown,
-          click: onClick,
-        },
-      })
+          onMousedown,
+          onClick
+        }
+      });
 
       return this.needsTile(scopedSlot)
         ? h(VListItem, tile, scopedSlot)
