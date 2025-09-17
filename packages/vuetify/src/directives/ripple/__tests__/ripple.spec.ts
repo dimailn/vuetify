5009cde6b9dd326c30c6e90310d35ca4f8c9b5b7
vladimir.ivanin <vladimir.ivanin@insales.ru>
2025-09-17 18:07:20 +0300

    обновлены тесты для директивы ripple

--- packages/vuetify/src/directives/ripple/__tests__/ripple.spec.ts
+++ packages/vuetify/src/directives/ripple/__tests__/ripple.spec.ts
@@ -1,36 +1,38 @@
-// Libraries
-import Vue from 'vue'
-
 // Directives
-import Ripple from '../'
+import { Ripple } from '../'
 
 // Utilities
 import {
   mount,
+  VueWrapper,
+  MountingOptions,
+  enableAutoUnmount,
 } from '@vue/test-utils'
+import { h, defineComponent, withDirectives } from 'vue'
 
 describe('ripple.ts', () => {
-  let mountFunction: (options?: object) => Wrapper<Vue>
+  let mountFunction: (options?: MountingOptions) => VueWrapper<any>
+
+  enableAutoUnmount(afterEach)
 
   beforeEach(() => {
     jest.useFakeTimers()
 
     mountFunction = (options = {}) => {
-      const testComponent = Vue.component('test', {
-        directives: {
-          Ripple,
-        },
-        render (h) {
-          const data = {
-            directives: [{
-              name: 'ripple',
-            }],
-          }
-          return h('div', data)
+      const testComponent = defineComponent({
+        render () {
+          return withDirectives(h('div'), [[Ripple, true]])
         },
       })
 
-      return mount(testComponent, options)
+      return mount(testComponent, {
+        global: {
+          directives: {
+            ripple: Ripple,
+          },
+        },
+        ...options,
+      })
     }
   })
 
@@ -42,43 +44,39 @@ describe('ripple.ts', () => {
     const wrapper = mountFunction()
 
     const div = wrapper.find('div')
-    expect(div.element._ripple.enabled).toBe(true)
+    expect(div.element._ripple?.enabled).toBe(true)
   })
 
-  it('should update element property reactively', () => {
-    const testComponent = Vue.component('test', {
+  it('should update element property reactively', async () => {
+    const testComponent = defineComponent({
       directives: {
         Ripple,
       },
       props: {
-        ripple: Boolean,
-        default: () => false,
+        ripple: {
+          type: Boolean,
+          default: false,
+        },
       },
-      render (h) {
-        const data = {
-          directives: [{
-            name: 'ripple',
-            value: this.ripple,
-          }],
-        }
-        return h('div', data)
+      render () {
+        return withDirectives(h('div'), [[Ripple, this.ripple]])
       },
     })
 
     const wrapper = mount(testComponent, {
-      propsData: {
+      props: {
         ripple: true,
       },
     })
 
     const div = wrapper.find('div')
-    expect(div.element._ripple.enabled).toBe(true)
+    expect(div.element._ripple?.enabled).toBe(true)
 
-    wrapper.setProps({ ripple: false })
-    expect(div.element._ripple.enabled).toBe(false)
+    await wrapper.setProps({ ripple: false })
+    expect(div.element._ripple?.enabled).toBe(false)
 
-    wrapper.setProps({ ripple: true })
-    expect(div.element._ripple.enabled).toBe(true)
+    await wrapper.setProps({ ripple: true })
+    expect(div.element._ripple?.enabled).toBe(true)
   })
 
   it('should trigger ripple on mousedown', () => {
