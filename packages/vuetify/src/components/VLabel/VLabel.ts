ee4e8f94096be500f0b848751ab956b077fe286c
vladimir.ivanin <vladimir.ivanin@insales.ru>
2025-09-17 19:05:33 +0300

    исправлена обработка содержимого слотов в компоненте VLabel

--- packages/vuetify/src/components/VLabel/VLabel.ts
+++ packages/vuetify/src/components/VLabel/VLabel.ts
@@ -60,6 +60,6 @@ export default mixins(Themeable).extend({
       ref: 'label',
     }, data)
 
-    return h('label', Colorable.methods.setTextColor(props.focused && props.color, newData), slots.default?.())
+    return h('label', Colorable.methods.setTextColor(props.focused && props.color, newData), slots.default ? slots.default() : [])
   },
 })
