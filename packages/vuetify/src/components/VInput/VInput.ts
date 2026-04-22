ae19b75fef407a235412460ebb55667ad17b73c3
vladimir.ivanin <vladimir.ivanin@insales.ru>
2026-04-22 12:13:58 +0300

    Исправляю эмит события change

--- packages/vuetify/src/components/VInput/VInput.ts
+++ packages/vuetify/src/components/VInput/VInput.ts
@@ -113,7 +113,7 @@ export default baseMixins.extend({
         this.lazyValue = val
         this.$emit(this.$_modelEvent, val)
 
-        if ('$_emitChangeEvent' in this) {
+        if (this.$options['$_emitChangeEvent']) {
           this.$emit('change', val)
         }
       }
