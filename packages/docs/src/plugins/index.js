61268b9c9f5918e7e517dd9c00444ba471fd87cc
vladimir.ivanin <vladimir.ivanin@insales.ru>
2026-07-10 09:58:33 +0300

    фикс загрузки шрифта

--- packages/docs/src/plugins/index.js
+++ packages/docs/src/plugins/index.js
@@ -4,10 +4,12 @@
  * Automatically included in `./src/main.js`
  */
 
+import { loadFonts } from './webfontloader'
 import { registerComponents } from './app'
 import { installHead } from './unhead'
 
 export function registerPlugins (app) {
   registerComponents(app)
+  loadFonts(app)
   return installHead(app)
 }
